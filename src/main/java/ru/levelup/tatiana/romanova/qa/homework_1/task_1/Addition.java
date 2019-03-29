e345066dc036b9a71d7f494a0781166882cfec3f
tatiana <tanya_rom@inbox.ru>
2019-03-29 14:37:18 +0300

    mvn

---
+++ src/main/java/ru/levelup/tatiana/romanova/qa/homework_1/task_1/Addition.java
@@ -0,0 +1,19 @@
+package ru.levelup.tatiana.romanova.qa.homework_1.task_1;
+
+public class Addition {
+
+    public long calcAddition(long numOne, long numTwo) {
+
+        return numOne + numTwo;
+    }
+
+    public int calcAddition(int numOne, int numTwo) {
+
+        return numOne + numTwo;
+    }
+
+    public double calcAddition(double numOne, double numTwo) {
+
+        return numOne + numTwo;
+    }
+}
---
+++ src/main/java/ru/levelup/tatiana/romanova/qa/homework_1/task_1/Numbers.java
@@ -0,0 +1,83 @@
+package ru.levelup.tatiana.romanova.qa.homework_1.task_1;
+
+public class Numbers {
+
+    private int intNumOne;
+    private int intNumTwo;
+    private double doubleNumOne;
+    private double doubleNumTwo;
+    private long longNumOne;
+    private long longNumTwo;
+    private int power;
+
+    public void setNumOne(int intNumOne) {
+
+        this.intNumOne = intNumOne;
+    }
+
+    public void setNumTwo(int intNumTwo) {
+
+        this.intNumTwo = intNumTwo;
+    }
+
+    public void setNumOne(double doubleNumOne) {
+
+        this.doubleNumOne = doubleNumOne;
+    }
+
+    public void setNumTwo(double doubleNumTwo) {
+
+        this.doubleNumTwo = doubleNumTwo;
+    }
+
+    public void setNumOne(long longNumOne) {
+
+        this.longNumOne = longNumOne;
+    }
+
+    public void setNumTwo(long longNumTwo) {
+
+        this.longNumTwo = longNumTwo;
+    }
+
+    public void setPower(int power) {
+
+        this.power = power;
+    }
+
+    public int getIntNumOne() {
+
+        return intNumOne;
+    }
+
+    public int getIntNumTwo() {
+
+        return intNumTwo;
+    }
+
+    public double getDoubleNumOne() {
+
+        return doubleNumOne;
+    }
+
+    public double getDoubleNumTwo() {
+
+        return doubleNumTwo;
+    }
+
+    public long getLongNumOne() {
+
+        return longNumOne;
+    }
+
+    public long getLongNumTwo() {
+
+        return longNumTwo;
+    }
+
+    public int getPower() {
+
+        return power;
+    }
+
+}
